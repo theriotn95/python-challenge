ef141919744f839f2c80e0c53590d82d2f7bbfa6
theriotn95 <theriotn95@gmail.com>
2020-02-21 21:33:17 -0800

    committing chnages to save my script

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,4 +1,23 @@
 #Starting the import of the election_data.csv file
 
-import cvs
+import csv
 import os
+
+# setting the path for the election_data.csv file 
+
+csv_PyPoll = os.path.join("Downloads","election_data.csv")
+
+# Set up lists to store your data 
+
+Candidate_names = []
+Candidates = []
+count = 0
+candidate_vote = []
+percent_won = []
+
+# Now we open the csv file with the path we set csv_PyPoll
+
+with open(csv_PyPoll, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimter=",")
+    csv_header = next(csvreader)
+
