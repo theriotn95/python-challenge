9dd62772206ca395e868e2ab7018026c1384bbfc
theriotn95 <theriotn95@gmail.com>
2020-02-21 23:04:45 -0800

    Almost have all my data to calcualte and print my final results

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,7 +3,7 @@
 import csv
 import os
 
-# setting the path for the election_data.csv file 
+# setting the output path for the election_data.csv file 
 
 csv_PyPoll = os.path.join("Downloads","election_data.csv")
 
@@ -11,13 +11,39 @@ csv_PyPoll = os.path.join("Downloads","election_data.csv")
 
 Candidate_names = []
 Candidates = []
-count = 0
-candidate_vote = []
+votes = 0
+Candidate_votes = []
 percent_won = []
 
 # Now we open the csv file with the path we set csv_PyPoll
 
 with open(csv_PyPoll, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimter=",")
-    csv_header = next(csvreader)
+# make sure to skip header
+    next(csvreader, None)
+
+# Start the for loop to iterate through our lists
+
+for row in csvreader:
+    # get the total number of votes
+    votes = votes + 1
+    # set candidate names to candidates
+    Candidates.append(row[2])
+#Now to get indivdual candiate names create a set from our Candidates List
+for a in set(Candidates):
+    Candidate_names.append(a)
+    # get the total number of votes per candidate with b
+    b = Candidates.count(a)
+    Candidate_votes.append(b)
+    # get the percent of the total votes for each candidate received 
+    c = (b/votes)*100
+    percent_won.append(c)
+
+
+
+
+
+
+
+
 
