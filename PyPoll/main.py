77a2d04d2f846827314be61c51a58e7372193d0b
theriotn95 <theriotn95@gmail.com>
2020-02-22 16:02:16 -0800

    fixed copy and paste errors along with grammatcial errors to get my poll results

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,44 +3,58 @@
 import csv
 import os
 
-# setting the output path for the election_data.csv file 
+# setting the path for the election_data.csv file 
 
-csv_PyPoll = os.path.join("Downloads","election_data.csv")
+csv_PyPoll = os.path.join("Desktop","Downloads","election_data.csv")
 
 # Set up lists to store your data 
 
-Candidate_names = []
-Candidates = []
+candidate_names = []
+candidates = []
 votes = 0
-Candidate_votes = []
+candidate_votes = []
 percent_won = []
 
 # Now we open the csv file with the path we set csv_PyPoll
 
 with open(csv_PyPoll, newline="") as csvfile:
-    csvreader = csv.reader(csvfile, delimter=",")
+    csvreader = csv.reader(csvfile, delimiter=",")
 # make sure to skip header
     next(csvreader, None)
 
 # Start the for loop to iterate through our lists
 
-for row in csvreader:
-    # get the total number of votes
-    votes = votes + 1
-    # set candidate names to candidates
-    Candidates.append(row[2])
-#Now to get indivdual candiate names create a set from our Candidates List
-for a in set(Candidates):
-    Candidate_names.append(a)
-    # get the total number of votes per candidate with b
-    b = Candidates.count(a)
-    Candidate_votes.append(b)
-    # get the percent of the total votes for each candidate received 
-    c = (b/votes)*100
-    percent_won.append(c)
+    for row in csvreader:
+        # get the total number of votes
+        votes = votes + 1
+        # set candidate names to candidates
+        candidates.append(row[2])
+    #Now to get indivdual candiate names create a set from our Candidates List
+    for a in set(candidates):
+        candidate_names.append(a)
+        # get the total number of votes per candidate with b
+        b = candidates.count(a)
+        candidate_votes.append(b)
+        # get the percent of the total votes for each candidate received 
+        c = (b/votes)*100
+        percent_won.append(c)
+
+#Prepare to print to temrinal and determine the winner
+
+winner_vote_count = max(candidate_votes)
+winner = candidate_names[candidate_votes.index(winner_vote_count)]
 
 
-
+print("-------------------------")
+print("Election Results")
+print("-------------------------")
+print("Total Votes: " + str(votes))
+print("-------------------------")
+for v in range(len(candidate_names)):
+            print(candidate_names[v] + ":" + str(percent_won[v]) +"% (" + str(candidate_votes[v])+ ")")
+print("-------------------------")
+print("The winner is: " + winner)
+print("-------------------------")
 
 
 
